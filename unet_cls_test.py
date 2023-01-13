a54c052f5d0058075f1a10159649f45b42ea0c4a
yzeng22 <zengxianyu18@gmail.com>
2023-01-13 17:21:39 -0500

    eval real

--- unet_cls_test.py
+++ unet_cls_test.py
@@ -11,6 +11,7 @@ from logger import logger
 
 parser = argparse.ArgumentParser(description="training unet and classfier")
 parser.add_argument("--data_dir", type=str)
+parser.add_argument("--gt_dir", type=str)
 parser.add_argument("--log_dir", type=str)
 parser.add_argument("--model_path", type=str)
 parser.add_argument("--image_size", type=int, default=256)
@@ -24,16 +25,20 @@ image_size = args.image_size
 batch_size = args.batch_size
 device = torch.device("cuda:0")
 
+def load_data_from_path(load_data_dir):
+    data = load_data(
+        data_dir=load_data_dir,
+        batch_size=batch_size,
+        image_size=image_size,
+        class_cond=False,
+        return_name=True,
+        deterministic=True,
+        return_loader=True
+    )
+    data = iter(data)
+    return data
+
 print("load data")
-data = load_data(
-    data_dir=data_dir,
-    batch_size=batch_size,
-    image_size=image_size,
-    class_cond=False,
-    return_name=True,
-    deterministic=True,
-    return_loader=True
-)
 
 print("create classifier")
 net_cls = torchvision.models.resnet34(pretrained=False)
@@ -43,23 +48,33 @@ net_cls.load_state_dict(torch.load(model_cls_path))
 net_cls.to(device)
 net_cls.eval()
 
-label_one = torch.Tensor([1]).to(device)[:,None]
-num_correct = 0
-num_all = 0
-data = iter(data)
-bdata = next(data, None)
-while bdata is not None:
-    sample, cond = bdata
-    sample = sample.to(device)
-    bsize,c,h,w = sample.size()
-    num_all += (bsize)
-    label = label_one.expand(bsize,-1)
-    t = torch.Tensor([dumt]*bsize).to(device)
-    with torch.no_grad():
-        pred = net_cls(sample)
-        pred = (pred>0).float()
-        num_correct += (pred==label).sum()
-        batch_acc = (pred==label).sum().float()/(bsize)
-    print(f"batch acc: {batch_acc}")
+def predict(label_gt, data):
+    num_correct = 0
+    num_all = 0
     bdata = next(data, None)
-print(f"TotalAcc: {float(num_correct)/num_all}")
+    while bdata is not None:
+        sample, cond = bdata
+        sample = sample.to(device)
+        bsize,c,h,w = sample.size()
+        num_all += (bsize)
+        label = label_gt.expand(bsize,-1)
+        t = torch.Tensor([dumt]*bsize).to(device)
+        with torch.no_grad():
+            pred = net_cls(sample)
+            pred = (pred>0).float()
+            num_correct += (pred==label).sum()
+            batch_acc = (pred==label).sum().float()/(bsize)
+        print(f"batch acc: {batch_acc}")
+        bdata = next(data, None)
+    return num_correct, num_all
+
+# fake images
+data = load_data_from_path(args.data_dir)
+label_one = torch.Tensor([1]).to(device)[:,None]
+num_correct, num_all = predict(label_one, data)
+
+# real images
+data = load_data_from_path(args.gt_dir)
+label_zero = torch.Tensor([0]).to(device)[:,None]
+num_correct_gt, num_all_gt = predict(label_zero, data)
+print(f"TotalAcc: {float(num_correct+num_correct_gt)/(num_all+num_all_gt)}")
