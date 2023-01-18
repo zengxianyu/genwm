ace8c2022b4eeaf95605e5cb2a1cccdb2ca52e30
Mo Zhou <cdluminate@gmail.com>
2023-01-18 14:35:09 -0500

    bugfix

--- figures/roc.sh
+++ figures/roc.sh
@@ -1,7 +1,7 @@
 python3 roc.py \
 	--ytrue data/ffhq_1bit_0.05/120000.ytrue.txt \
 	--yscore data/ffhq_1bit_0.05/120000.yscores.txt \
-	--title 'Stage 1: ROC for $F$' --color crimson \
+	--title 'Stage 1: ROC for $W$' --color crimson \
 	--save stage1-roc.pdf
 python3 roc.py \
 	--ytrue data/ffhq_1bit_0.05_adm/output.ytrue.txt \
