9d4e07e6b0cb6879ce71250c8cd5dfc193949c37
Mo Zhou <cdluminate@gmail.com>
2023-01-18 14:27:55 -0500

    figures: update roc.sh for new data

--- figures/roc.sh
+++ figures/roc.sh
@@ -1,10 +1,16 @@
 python3 roc.py \
 	--ytrue data/ffhq_1bit_0.05/120000.ytrue.txt \
 	--yscore data/ffhq_1bit_0.05/120000.yscores.txt \
-	--title 'Stage 1: ROC for $F$' --color crimson
-	#--save stage1-roc.pdf \
+	--title 'Stage 1: ROC for $F$' --color crimson \
+	--save stage1-roc.pdf
 python3 roc.py \
 	--ytrue data/ffhq_1bit_0.05_adm/output.ytrue.txt \
 	--yscore data/ffhq_1bit_0.05_adm/output.yscores.txt \
-	--title 'Stage 2: ROC for ADM' --color slateblue
-	#--save stage2-roc-adm.pdf \
+	--title 'Stage 2: ROC for ADM' --color slateblue \
+	--save stage2-roc-adm.pdf
+python3 roc.py \
+	--ytrue data/ffhq_1bit_0.05_ldm/img.ytrue.txt \
+	--yscore data/ffhq_1bit_0.05_ldm/img.yscores.txt \
+	--title 'Stage 2: ROC for LDM' --color slateblue \
+	--save stage2-roc-ldm.pdf
+
