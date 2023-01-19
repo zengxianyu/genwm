7f520d662c277821225d91aa62fb48c6ca69f668
Mo Zhou <cdluminate@gmail.com>
2023-01-18 20:23:31 -0500

    roc.py: support --seen_y{true,score}

--- figures/roc.py
+++ figures/roc.py
@@ -19,6 +19,8 @@ ag.add_argument('--yscore', type=str, required=True,
 ag.add_argument('--color', type=str, default='crimson')
 ag.add_argument('--title', type=str, default='')
 ag.add_argument('--save', type=str, default=None)
+ag.add_argument('--seen_ytrue', type=str, default=None, help='optional')
+ag.add_argument('--seen_yscore', type=str, default=None, help='optional')
 ag = ag.parse_args()
 console.print(ag)
 
@@ -36,10 +38,21 @@ console.print('TPR', tpr)
 # plot
 fig = plt.figure(figsize=(5.4, 4.8))
 plt.plot(fpr, tpr, marker='p', color=ag.color)
-plt.plot([0,1], [0,1], 'k:')
+plt.plot([0,1], [0,1], 'k:', label='_nolegend_')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title(ag.title)
+
+legends = ['ours']
+if ag.seen_ytrue and ag.seen_yscore:
+	legends.append('seen')
+	seen_ytrue = np.loadtxt(ag.seen_ytrue)
+	seen_yscore = np.loadtxt(ag.seen_yscore)
+	fpr, tpr, _ = roc_curve(seen_ytrue, seen_yscore, pos_label=1)
+	plt.plot(fpr, tpr, marker='+')
+
+if len(legends) > 1:
+	plt.legend(legends)
 
 plt.tight_layout()
 
