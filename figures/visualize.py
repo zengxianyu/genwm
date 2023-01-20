44b9e96224006face1718f787bd598f4a906003c
Mo Zhou <cdluminate@gmail.com>
2023-01-19 23:08:56 -0500

    visualize.py: overhaul plotting code for W vis

--- figures/visualize.py
+++ figures/visualize.py
@@ -14,123 +14,88 @@ ag.add_argument('-s', '--save', type=str, default=None)
 ag.add_argument('which', type=str, choices=['w', 'g'])
 ag = ag.parse_args()
 
+# helpers
+def w_showtriple(ax1, gt, ax2, w, ax3, *, ylabel=None, title=False):
+    __CMAP__ = 'RdPu'
+    # 1
+    im = Image.open(gt)
+    ax1.imshow(im)
+    if title:
+        ax1.set_title('Ground Truth $x$')
+    if ylabel is not None:
+        ax1.set_ylabel(ylabel)
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax1.spines['top'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    ax1.spines['bottom'].set_visible(False)
+    ax1.spines['left'].set_visible(False)
+    # 2
+    imw = Image.open(w)
+    ax2.imshow(imw)
+    ax2.axis(False)
+    if title:
+        ax2.set_title('Watermarked $\\hat{x}$')
+    # 3
+    kappa = np.linalg.norm(np.array(imw) - np.array(im), axis=2)
+    kappa = kappa / kappa.max()
+    kappa = (kappa * 255).astype(np.uint8)
+    imk = Image.fromarray(kappa)
+    ax3.imshow(imk, cmap=__CMAP__)
+    #plt.colorbar(ax=ax3)
+    ax3.axis(False)
+    if title:
+        ax3.set_title('Normalized $\\kappa$')
+
+
 # draw
 if ag.which == 'w':
-	# stage 1
-	fig = plt.figure(figsize=(14.4,20))
+    # stage 1
+    fig = plt.figure(figsize=(14.4,20))
 
-	ax = plt.subplot(4,3,1)
-	im = Image.open('imgs/ffhq/01627_GT.png')
-	ax.imshow(im)
-	#ax.axis(False)
-	ax.set_title('Ground Truth $x$')
-	ax.set_ylabel('FFHQ')
-	ax.set_xticklabels([])
-	ax.set_yticklabels([])
-	ax.set_xticks([])
-	ax.set_yticks([])
+    ax1 = plt.subplot(4,3,1)
+    ax2 = plt.subplot(4,3,2)
+    ax3 = plt.subplot(4,3,3)
+    w_showtriple(ax1, 'imgs/ffhq/01627_GT.png',
+                 ax2, 'imgs/ffhq/01627_W.png',
+                 ax3,
+                 ylabel='FFHQ',
+                 title=True)
 
-	ax = plt.subplot(4,3,2)
-	imw = Image.open('imgs/ffhq/01627_W.png')
-	ax.imshow(im)
-	ax.axis(False)
-	ax.set_title('Watermarked $\\hat{x}$')
+    ax4 = plt.subplot(4,3,4)
+    ax5 = plt.subplot(4,3,5)
+    ax6 = plt.subplot(4,3,6)
+    w_showtriple(ax4, 'imgs/ffhq/28170_GT.png',
+                 ax5, 'imgs/ffhq/28170_W.png',
+                 ax6,
+                 ylabel='FFHQ')
 
-	ax = plt.subplot(4,3,3)
-	kappa = np.linalg.norm(np.array(imw) - np.array(im), axis=2)
-	kappa = kappa / kappa.max()
-	plt.pcolormesh(np.flipud(kappa), cmap='plasma')
-	#plt.pcolormesh(kappa, cmap='plasma')
-	plt.colorbar()
-	ax.axis(False)
-	ax.axis('square')
-	ax.set_title('Normalized $\\kappa$')
+    ax7 = plt.subplot(4,3,7)
+    ax8 = plt.subplot(4,3,8)
+    ax9 = plt.subplot(4,3,9)
+    w_showtriple(ax7, 'imgs/imagenet/ILSVRC2012_val_00025488_GT.JPEG',
+                 ax8, 'imgs/imagenet/ILSVRC2012_val_00025488_W.JPEG',
+                 ax9,
+                 ylabel='ImageNet')
 
-	ax = plt.subplot(4,3,4)
-	im = Image.open('imgs/ffhq/28170_GT.png')
-	ax.imshow(im)
-	#ax.axis(False)
-	ax.set_ylabel('FFHQ')
-	ax.set_xticklabels([])
-	ax.set_yticklabels([])
-	ax.set_xticks([])
-	ax.set_yticks([])
-
-	ax = plt.subplot(4,3,5)
-	imw = Image.open('imgs/ffhq/28170_W.png')
-	ax.imshow(im)
-	ax.axis(False)
-
-	ax = plt.subplot(4,3,6)
-	kappa = np.linalg.norm(np.array(imw) - np.array(im), axis=2)
-	kappa = kappa / kappa.max()
-	plt.pcolormesh(np.flipud(kappa), cmap='plasma')
-	plt.colorbar()
-	ax.axis(False)
-	ax.axis('square')
-
-	ax = plt.subplot(4,3,7)
-	im = Image.open('imgs/imagenet/ILSVRC2012_val_00025488_GT.JPEG')
-	ax.imshow(im)
-	#ax.axis(False)
-	ax.set_ylabel('ImageNet')
-	ax.set_xticklabels([])
-	ax.set_yticklabels([])
-	ax.set_xticks([])
-	ax.set_yticks([])
-
-	ax = plt.subplot(4,3,8)
-	imw = Image.open('imgs/imagenet/ILSVRC2012_val_00025488_W.JPEG')
-	ax.imshow(im)
-	ax.axis(False)
-
-	ax = plt.subplot(4,3,9)
-	kappa = np.linalg.norm(np.array(imw) - np.array(im), axis=2)
-	kappa = kappa / kappa.max()
-	plt.pcolormesh(np.flipud(kappa), cmap='plasma')
-	#plt.pcolormesh(kappa, cmap='plasma')
-	plt.colorbar()
-	ax.axis(False)
-	ax.axis('square')
-
-	ax = plt.subplot(4,3,10)
-	im = Image.open('imgs/imagenet/ILSVRC2012_val_00027351_GT.JPEG')
-	ax.imshow(im)
-	#ax.axis(False)
-	ax.set_ylabel('ImageNet')
-	ax.set_xticklabels([])
-	ax.set_yticklabels([])
-	ax.set_xticks([])
-	ax.set_yticks([])
-
-	ax = plt.subplot(4,3,11)
-	imw = Image.open('imgs/imagenet/ILSVRC2012_val_00027351_W.JPEG')
-	ax.imshow(im)
-	ax.axis(False)
-
-	ax = plt.subplot(4,3,12)
-	kappa = np.linalg.norm(np.array(imw) - np.array(im), axis=2)
-	kappa = kappa / kappa.max()
-	#kim = (kappa*255).astype(np.uint8).reshape(256,256,1)
-	#print(kim)
-	#ax.imshow(kappa)
-	plt.pcolormesh(np.flipud(kappa), cmap='plasma')
-	#plt.pcolormesh(np.fliplr(kappa.T), cmap='plasma')
-	plt.colorbar()
-	ax.axis(False)
-	ax.axis('square')
-
-	#plt.figure()
-	#plt.imshow(kappa)
+    ax10 = plt.subplot(4,3,10)
+    ax11 = plt.subplot(4,3,11)
+    ax12 = plt.subplot(4,3,12)
+    w_showtriple(ax10, 'imgs/imagenet/ILSVRC2012_val_00027351_GT.JPEG',
+                 ax11, 'imgs/imagenet/ILSVRC2012_val_00027351_W.JPEG',
+                 ax12,
+                 ylabel='ImageNet')
 
 elif ag.which == 'g':
 
-	raise NotImplementedError
+    raise NotImplementedError
 
 if ag.save is None:
-	plt.show()
+    plt.show()
 else:
-	plt.savefig(ag.save)
+    plt.savefig(ag.save)
 
 #figures/imgs/ffhq/000057_stylegan2.png
 #figures/imgs/ffhq/000266_stylegan2.png
