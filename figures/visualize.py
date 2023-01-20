d4bb6559415fb2e8f2a633e3574f357c7b0014f4
Mo Zhou <cdluminate@gmail.com>
2023-01-19 23:25:50 -0500

    transpose vis of W

--- figures/visualize.py
+++ figures/visualize.py
@@ -5,7 +5,8 @@ from PIL import Image
 import argparse
 
 # configs
-font = {'family': 'monospace', 'weight': 'normal', 'size': 22}
+__FONTSIZE__ = 32
+font = {'family': 'monospace', 'weight': 'normal', 'size': __FONTSIZE__}
 matplotlib.rc('font', **font)
 
 # arguments
@@ -15,15 +16,15 @@ ag.add_argument('which', type=str, choices=['w', 'g'])
 ag = ag.parse_args()
 
 # helpers
-def w_showtriple(ax1, gt, ax2, w, ax3, *, ylabel=None, title=False):
+def w_showtriple(ax1, gt, ax2, w, ax3, *, ylabel=None, title=None):
     __CMAP__ = 'RdPu'
     # 1
     im = Image.open(gt)
     ax1.imshow(im)
-    if title:
-        ax1.set_title('Ground Truth $x$')
+    if title is not None:
+        ax1.set_title(title, fontsize=__FONTSIZE__)
     if ylabel is not None:
-        ax1.set_ylabel(ylabel)
+        ax1.set_ylabel(ylabel[0], fontsize=__FONTSIZE__)
     ax1.set_xticklabels([])
     ax1.set_yticklabels([])
     ax1.set_xticks([])
@@ -35,59 +36,70 @@ def w_showtriple(ax1, gt, ax2, w, ax3, *, ylabel=None, title=False):
     # 2
     imw = Image.open(w)
     ax2.imshow(imw)
-    ax2.axis(False)
-    if title:
-        ax2.set_title('Watermarked $\\hat{x}$')
+    if ylabel is not None:
+        ax2.set_ylabel(ylabel[1], fontsize=__FONTSIZE__)
+    ax2.set_xticklabels([])
+    ax2.set_yticklabels([])
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    ax2.spines['top'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+    ax2.spines['bottom'].set_visible(False)
+    ax2.spines['left'].set_visible(False)
     # 3
     kappa = np.linalg.norm(np.array(imw) - np.array(im), axis=2)
     kappa = kappa / kappa.max()
     kappa = (kappa * 255).astype(np.uint8)
     imk = Image.fromarray(kappa)
     ax3.imshow(imk, cmap=__CMAP__)
-    #plt.colorbar(ax=ax3)
-    ax3.axis(False)
-    if title:
-        ax3.set_title('Normalized $\\kappa$')
+    if ylabel is not None:
+        ax3.set_ylabel(ylabel[2], fontsize=__FONTSIZE__)
+    ax3.set_xticklabels([])
+    ax3.set_yticklabels([])
+    ax3.set_xticks([])
+    ax3.set_yticks([])
+    ax3.spines['top'].set_visible(False)
+    ax3.spines['right'].set_visible(False)
+    ax3.spines['bottom'].set_visible(False)
+    ax3.spines['left'].set_visible(False)
 
 
 # draw
 if ag.which == 'w':
     # stage 1
-    fig = plt.figure(figsize=(14.4,20))
+    fig = plt.figure(figsize=(20, 14.4))
 
-    ax1 = plt.subplot(4,3,1)
-    ax2 = plt.subplot(4,3,2)
-    ax3 = plt.subplot(4,3,3)
+    ax1 = plt.subplot(3,4,1)
+    ax2 = plt.subplot(3,4,5)
+    ax3 = plt.subplot(3,4,9)
     w_showtriple(ax1, 'imgs/ffhq/01627_GT.png',
                  ax2, 'imgs/ffhq/01627_W.png',
                  ax3,
-                 ylabel='FFHQ',
-                 title=True)
-
-    ax4 = plt.subplot(4,3,4)
-    ax5 = plt.subplot(4,3,5)
-    ax6 = plt.subplot(4,3,6)
+                 ylabel=['Ground Truth $x$',
+                        'Watermarked $\\hat{x}$',
+                        'Watermark $\\kappa$',],
+                 title='FFHQ')
+    ax4 = plt.subplot(3,4,2)
+    ax5 = plt.subplot(3,4,6)
+    ax6 = plt.subplot(3,4,10)
     w_showtriple(ax4, 'imgs/ffhq/28170_GT.png',
                  ax5, 'imgs/ffhq/28170_W.png',
                  ax6,
-                 ylabel='FFHQ')
-
-    ax7 = plt.subplot(4,3,7)
-    ax8 = plt.subplot(4,3,8)
-    ax9 = plt.subplot(4,3,9)
+                 title='FFHQ')
+    ax7 = plt.subplot(3,4,3)
+    ax8 = plt.subplot(3,4,7)
+    ax9 = plt.subplot(3,4,11)
     w_showtriple(ax7, 'imgs/imagenet/ILSVRC2012_val_00025488_GT.JPEG',
                  ax8, 'imgs/imagenet/ILSVRC2012_val_00025488_W.JPEG',
                  ax9,
-                 ylabel='ImageNet')
-
-    ax10 = plt.subplot(4,3,10)
-    ax11 = plt.subplot(4,3,11)
-    ax12 = plt.subplot(4,3,12)
+                 title='ImageNet')
+    ax10 = plt.subplot(3,4,4)
+    ax11 = plt.subplot(3,4,8)
+    ax12 = plt.subplot(3,4,12)
     w_showtriple(ax10, 'imgs/imagenet/ILSVRC2012_val_00027351_GT.JPEG',
                  ax11, 'imgs/imagenet/ILSVRC2012_val_00027351_W.JPEG',
                  ax12,
-                 ylabel='ImageNet')
-
+                 title='ImageNet')
     plt.tight_layout()
 
 elif ag.which == 'g':
