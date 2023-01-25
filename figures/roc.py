a81171c75d927fcefff6e680fd49b2e887b6eeca
Mo Zhou <cdluminate@gmail.com>
2023-01-24 23:24:29 -0500

    figures: update roc.py

--- figures/roc.py
+++ figures/roc.py
@@ -23,6 +23,10 @@ ag.add_argument('--seen_ytrue', type=str, default=None, help='optional')
 ag.add_argument('--seen_yscore', type=str, default=None, help='optional')
 ag.add_argument('--unseen_ytrue', type=str, default=None, help='optional')
 ag.add_argument('--unseen_yscore', type=str, default=None, help='optional')
+ag.add_argument('--wang_ytrue', type=str, default=None)
+ag.add_argument('--wang_yscore', type=str, default=None)
+ag.add_argument('--grag_ytrue', type=str, default=None)
+ag.add_argument('--grag_yscore', type=str, default=None)
 ag = ag.parse_args()
 console.print(ag)
 
@@ -48,7 +52,21 @@ if ag.unseen_ytrue and ag.unseen_yscore:
     unseen_ytrue = np.loadtxt(ag.unseen_ytrue)
     unseen_yscore = np.loadtxt(ag.unseen_yscore)
     fpr, tpr, _ = roc_curve(unseen_ytrue, unseen_yscore, pos_label=1)
-    plt.plot(fpr, tpr, marker='.')
+    plt.plot(fpr, tpr, marker=None)
+
+if ag.wang_ytrue and ag.wang_yscore:
+    legends.append('Wang et al.')
+    wang_ytrue = np.loadtxt(ag.wang_ytrue)
+    wang_yscore = np.loadtxt(ag.wang_yscore)
+    fpr, tpr, _ = roc_curve(wang_ytrue, wang_yscore, pos_label=1)
+    plt.plot(fpr, tpr, marker=None)
+
+if ag.grag_ytrue and ag.grag_yscore:
+    legends.append('Gragnaniello et al.')
+    grag_ytrue = np.loadtxt(ag.grag_ytrue)
+    grag_yscore = np.loadtxt(ag.grag_yscore)
+    fpr, tpr, _ = roc_curve(grag_ytrue, grag_yscore, pos_label=1)
+    plt.plot(fpr, tpr, marker=None)
 
 if ag.ytrue and ag.yscore:
     fpr, tpr, thresholds = roc_curve(y_true, y_score, pos_label=1)
@@ -61,7 +79,8 @@ if ag.ytrue and ag.yscore:
     plt.title(ag.title)
     legends.append('ours')
 
-plt.legend(legends)
+
+plt.legend(legends, fontsize=12)
 plt.tight_layout()
 
 # dump
